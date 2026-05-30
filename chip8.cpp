94d5ea6238bce5ee4e7161b15b41116dd8534d7a
zeptodan <muhammadannas138@gmail.com>
2026-05-31 00:54:42 +0500

    adding fetch decode execute cycle

--- chip8.cpp
+++ chip8.cpp
@@ -1,5 +1,38 @@
 #include<fstream>
 #include"chip8.h"
 void Chip8::load_rom(std::string file_path){
-    std::ifstream file(file_path);
+    std::ifstream file(file_path, std::ios::binary);
+    file.read(reinterpret_cast<char*>(ram.data() + ROM_START),ram.size() - ROM_START);
+    return;
+}
+Chip8::Chip8() : regs{}, ram{}, index{}, pc{}, d_time{},s_time{} {}
+uint16_t Chip8::fetch(){
+    uint16_t op =  (ram[pc] >> 8) | ram[pc + 1];
+    return op;
+}
+void Chip8::decode_execute(){
+    uint16_t op = fetch();
+    uint16_t X = op | 0x0F00;
+    uint16_t Y = op | 0x00F0;
+    uint16_t N = op | 0x000F;
+    uint16_t NN = op | 0x00FF;
+    uint16_t NNN = op | 0x0FFF;
+    switch (op | 0xF000)
+    {
+        case 0x0000:
+            break;
+        case 0x1000:
+            break;
+        case 0x6000:
+            break;
+        case 0x7000:
+            break;
+        case 0xA000:
+            break;
+        case 0xD000:
+            break;
+        default:
+            break;
+    }
+
 }
--- chip8.h
+++ chip8.h
@@ -4,14 +4,19 @@
 #include<string>
 #define RAM_SIZE 4096
 #define TOT_REGS 16
+#define ROM_START 512
 class Chip8 {
-    std::array<int8_t,RAM_SIZE> ram[RAM_SIZE];
-    std::array<int8_t,TOT_REGS> regs[TOT_REGS];
-    std::stack<int16_t> stack;
-    int16_t pc;
-    int16_t index;
-    int8_t d_time;
-    int8_t s_time;
+    private:
+    std::array<uint8_t,RAM_SIZE> ram;
+    std::array<uint8_t,TOT_REGS> regs;
+    std::stack<uint16_t> stack;
+    uint16_t pc;
+    uint16_t index;
+    uint8_t d_time;
+    uint8_t s_time;
+    public:
+    Chip8();
     void load_rom(std::string file_path);
-    
+    uint16_t fetch();
+    void decode_execute();
 };
